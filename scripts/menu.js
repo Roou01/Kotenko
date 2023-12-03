573f9104d4c062b2e12a5df7741b1d7f245105e1
Anton Bondarenko <77900776+Roou01@users.noreply.github.com>
2023-12-03 22:26:17 +0300

    Delete scripts directory

--- scripts/menu.js
+++
@@ -1,8 +0,0 @@
-const section = document
-  .getElementById("analisys-section")
-  .getBoundingClientRect().top;
-console.log(section);
-
-document.getElementById("analisys-link").addEventListener("click", () => {
-  window.scrollTo(0, section);
-});
